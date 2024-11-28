967c9923717ecdd2bde7d0ed3bf2bd10409bb275
Yashgupta9330 <yaahg342@gmail.com>
2024-11-28 19:16:18 +0530

    added search component

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,22 @@
+import { useState } from 'react';
+import SearchBar from '../components/search';
+
 const Home = () => {
+  const [searchResults, setSearchResults] = useState<any[]>([]);
+
+  const handleSearchResult = (results: any[]) => {
+    setSearchResults(results);
+  };
+
   return (
-    <div>Hello!</div>
-  );
+    <div className="container mx-auto mt-4">
+      <SearchBar
+        placeholder="Search for a committee..."
+        searchEndpoint="http://127.0.0.1:8000/api/v1/owasp/search/committee"
+        onSearchResult={handleSearchResult}
+      />
+    </div>
+  )
 };
 
 export default Home;
